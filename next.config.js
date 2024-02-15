338cef639d3f5e4d595a29e75ffee79073bab6ed
Hector Barazorda <ph.barazorda@gmail.com>
2024-02-15 15:00:13 -0500

    update 1 file and delete 1 file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  "experimental": {
-    "serverActions": true
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil'
+    })
+    return config
   }
 }
-
-module.exports = nextConfig
