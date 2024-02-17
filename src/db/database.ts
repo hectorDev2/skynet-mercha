dae641252bf1caf2bd822ef60d8f9da930421ed2
Hector Barazorda <ph.barazorda@gmail.com>
2024-02-17 13:47:57 -0500

    fix: :art: prices products

--- src/db/database.ts
+++ src/db/database.ts
@@ -3,7 +3,7 @@ export const tShirts = [
     id: "1",
     name: "Polos Anime",
     category: "anime",
-    price: 25,
+    price: 50.0,
     description:
       "Sumérgete en el fascinante mundo del anime con nuestra colección de polos inspirados en tus series y personajes favoritos. Desde clásicos atemporales hasta los últimos éxitos de la animación japonesa, encontrarás diseños únicos que reflejan tu pasión por el anime.",
     image: [
@@ -17,7 +17,7 @@ export const tShirts = [
     id: "2",
     name: "Polos Counter Strike",
     category: "counter",
-    price: 20,
+    price: 50.0,
     description:
       "Únete a la acción trepidante y la estrategia táctica con nuestra colección de polos inspirados en el legendario juego Counter-Strike. Desde la emblemática insignia del Counter-Terrorist hasta el icónico logo del Terrorist, nuestros diseños capturan la esencia de este juego de disparos en primera persona que ha cautivado a millones de jugadores en todo el mundo.",
     image: [
@@ -30,7 +30,7 @@ export const tShirts = [
     id: "3",
     name: "Polos Doteros",
     category: "dota",
-    price: 18,
+    price: 50.0,
     description:
       "Sumérgete en el emocionante mundo de Dota con nuestra colección de polos inspirados en este popular juego de estrategia en tiempo real. Desde los héroes más icónicos hasta los diseños emblemáticos del mapa, nuestros polos capturan la esencia del combate épico y la competencia estratégica que define a Dota",
     image: [
@@ -44,7 +44,7 @@ export const tShirts = [
     id: "4",
     name: "God Of War",
     category: "god-of-war",
-    price: 30,
+    price: 50.0,
     description:
       "Embárcate en una aventura épica inspirada en la mitología griega con nuestra colección de polos de God of War. Con diseños que rinden homenaje al valiente guerrero espartano, Kratos, y a las criaturas míticas que pueblan su mundo, nuestros polos capturan la intensidad y la grandeza de esta aclamada serie de videojuegos. ",
     image: [
@@ -59,7 +59,7 @@ export const tShirts = [
     id: "5",
     name: "Half life",
     category: "Half_life",
-    price: 35,
+    price: 50.0,
     description:
       " Sumérgete en el mundo innovador y lleno de suspense de Half-Life con nuestra colección de polos inspirados en esta icónica serie de videojuegos. Desde el emblemático logotipo hasta diseños que rinden homenaje a personajes como Gordon Freeman y criaturas como los headcrabs, nuestros polos capturan la esencia de la revolucionaria franquicia de Valve.",
     image: ["/polos/half_life/1.png", "/polos/half_life/2.png"],
@@ -68,7 +68,7 @@ export const tShirts = [
     id: "6",
     name: "metal slug",
     category: "metal_slug",
-    price: 40,
+    price: 50.0,
     description:
       " Únete a la batalla contra el malvado General Morden y su ejército rebelde con nuestra colección de polos inspirados en la clásica serie de videojuegos Metal Slug. Con diseños que capturan la acción explosiva, los icónicos vehículos y los valientes héroes de la Resistencia, nuestros polos son el complemento perfecto para los fanáticos de la intensa acción arcade.",
     image: ["/polos/metal_slug/1.png"],
@@ -77,7 +77,7 @@ export const tShirts = [
     id: "7",
     name: "star craft",
     category: "star_craft",
-    price: 22,
+    price: 50.0,
 
     description:
       "Demuestra tu lealtad a la legendaria saga de videojuegos con nuestra selección de polos inspirados en el universo de StarCraft. Ya sea que te alinees con los terran, zerg o protoss, nuestros diseños te permitirán llevar tu amor por el juego a otro nivel.",
@@ -91,7 +91,7 @@ export const tShirts = [
     id: "8",
     name: "Nintendo",
     category: "nintendo",
-    price: 28,
+    price: 50.0,
     description:
       ": Revive la nostalgia y la emoción de tus juegos favoritos de Nintendo con nuestra línea de polos dedicada a esta icónica compañía de videojuegos. Desde Mario y Zelda hasta Pokémon y Splatoon, nuestra colección ofrece una amplia gama de opciones para los fans de todas las edades.",
     image: [
@@ -104,7 +104,7 @@ export const tShirts = [
     id: "9",
     name: "Modelo exclusivo",
     category: "exclusive",
-    price: 80.0,
+    price: 50.0,
     description:
       ": Revive la nostalgia y la emoción de tus juegos favoritos de Nintendo con nuestra línea de polos dedicada a esta icónica compañía de videojuegos. Desde Mario y Zelda hasta Pokémon y Splatoon, nuestra colección ofrece una amplia gama de opciones para los fans de todas las edades.",
     image: [
