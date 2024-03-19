7c9787be4dc9a64ec98f4c6b8512f5463403ae24
Hector Barazorda <ph.barazorda@gmail.com>
2024-03-18 19:23:45 -0500

    feat: :fire: update page dashboard link submit

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -72,6 +72,7 @@ const Login = () => {
         <Button
           className='w-full'
           color='success'
+          onClick={onSubmit}
           endContent={
             <svg
               xmlns='http://www.w3.org/2000/svg'
