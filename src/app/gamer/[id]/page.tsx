1ea53108cf0b24b8acc994031cdd4be3f875ba55
Hector Barazorda <ph.barazorda@gmail.com>
2024-03-22 11:53:15 -0500

    feat: :art: subcategory page

---
+++ src/app/gamer/[id]/page.tsx
@@ -0,0 +1,23 @@
+import { features } from '@/app/escultura/Data'
+import { Features } from '@/app/components/shared/landing/Main'
+import { Layout } from '@/app/layout/Layout'
+import ProductsContainer from '../components/ProductsContainer'
+
+const GamerPage = () => {
+  return (
+    <Layout>
+      <div className='my-[100px]'>
+        <ProductsContainer />
+        <Features
+          theme='secondary'
+          subtitle='Nuestras polos divertidos y originales'
+          title='Todo tipo de modelos de '
+          stringColor='polos para parejas'
+          features={features}
+        />
+      </div>
+    </Layout>
+  )
+}
+
+export default GamerPage
