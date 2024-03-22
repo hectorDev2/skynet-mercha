942640880d2b4b5ff09abe1b8d1db150e679b5c4
Hector Barazorda <ph.barazorda@gmail.com>
2024-03-22 11:53:02 -0500

    feat: :art: subcategory page

---
+++ src/app/anime/components/ProductsContainer.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { Products } from '@/app/components/containers/Products'
+import { getTshirtsParams } from '@/utils/fetch'
+import { useParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+const ProductsContainer = () => {
+  const [products, setProducts] = useState<any>()
+  const { id } = useParams()
+
+  // getTshirts()
+  useEffect(() => {
+    getTshirtsParams(1).then(res => {
+      setProducts(
+        res?.tshirts?.filter((tshirt: any) => tshirt?.subcategory === id)
+      )
+    })
+    return () => {
+      setProducts([])
+    }
+  }, [])
+
+  return (
+    <>
+      <Products tshirts={products} color='black' />
+    </>
+  )
+}
+
+export default ProductsContainer
