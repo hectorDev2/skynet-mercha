f15bd42232239c5c30be952408b1d81ab8866b80
Hector Barazorda <ph.barazorda@gmail.com>
2024-03-26 13:23:25 -0500

    feat: :art: prisma changes

--- src/app/musica/[id]/page.tsx
+++ src/app/musica/[id]/page.tsx
@@ -1,23 +1,23 @@
-import { features } from '@/app/escultura/Data'
-import { Features } from '@/app/components/shared/landing/Main'
-import { Layout } from '@/app/layout/Layout'
-import ProductsContainer from '../components/ProductsContainer'
+import { features } from "@/app/escultura/Data";
+import { Features } from "@/app/components/shared/landing/Main";
+import { Layout } from "@/app/layout/Layout";
+import ProductsContainer from "../components/ProductsSubcategories";
 
 const GamerPage = () => {
   return (
     <Layout>
-      <div className='my-[100px]'>
+      <div className="my-[100px]">
         <ProductsContainer />
         <Features
-          theme='secondary'
-          subtitle='Nuestras polos divertidos y originales'
-          title='Todo tipo de modelos de '
-          stringColor='polos para parejas'
+          theme="secondary"
+          subtitle="Nuestras polos divertidos y originales"
+          title="Todo tipo de modelos de "
+          stringColor="polos para parejas"
           features={features}
         />
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default GamerPage
+export default GamerPage;
