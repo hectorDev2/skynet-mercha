c00cc99bd0f800eb41eb66b070cd21903178a0d1
Hector Barazorda <ph.barazorda@gmail.com>
2024-03-27 13:30:46 -0500

    feat: :art: redirect dashboard default

--- next.config.js
+++ next.config.js
@@ -1,18 +1,28 @@
 module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.externals.push({
+  webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+    config.externals.push ({
       'utf-8-validate': 'commonjs utf-8-validate',
-      bufferutil: 'commonjs bufferutil'
-    })
-    return config
+      bufferutil: 'commonjs bufferutil',
+    });
+    return config;
   },
   images: {
     domains: [
       'res.cloudinary.com',
       'avatars.githubusercontent.com',
-      'lh3.googleusercontent.com'
-    ]
+      'lh3.googleusercontent.com',
+    ],
   },
 
-  noImplicitAny: false
-}
+  noImplicitAny: false,
+
+  async redirects () {
+    return [
+      {
+        source: '/dashboard',
+        destination: '/dashboard/tshirts', // Redirige a /dashboard/tshirts
+        permanent: false, // Cambia a true si deseas una redirección permanente (308)
+      },
+    ];
+  },
+};
