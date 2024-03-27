e4ea6b8768cc1970fd8d455cb46d7deb493aac4e
Hector Barazorda <ph.barazorda@gmail.com>
2024-03-27 09:11:02 -0500

    feat: :zap: new route shoes

---
+++ src/app/api/shoes/route.ts
@@ -0,0 +1,68 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const paramCategory = searchParams.get("category");
+
+  const shoes = await prisma.shoe.findMany({
+    include: {
+      images: true,
+    },
+    orderBy: [
+      {
+        id: "desc",
+      },
+    ],
+    where: {
+      category: {
+        contains: paramCategory ? paramCategory : "",
+      },
+    },
+  });
+
+  return NextResponse.json({
+    shoes,
+  });
+}
+
+export async function POST(request: Request) {
+  const {
+    name,
+    description,
+    price,
+    images,
+    tag,
+    category,
+    subcategory,
+    label,
+  } = await request.json();
+
+  const newTshirt = await prisma.shoe.create({
+    data: {
+      name,
+      description,
+      price,
+      category,
+      subcategory,
+      tag,
+      label,
+    },
+  });
+
+  await Promise.all(
+    images?.map(async (url: string) => {
+      await prisma.image.create({
+        data: {
+          url: url,
+          tshirtId: newTshirt.id,
+        },
+      });
+    })
+  );
+
+  return NextResponse.json({
+    newTshirt,
+    images,
+  });
+}
