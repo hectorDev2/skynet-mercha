497190f67acec2757bbe7974587bd149a8dbb963
Hector Barazorda <ph.barazorda@gmail.com>
2024-04-01 12:42:20 -0500

    feat: :art: prototype jacket page

---
+++ src/app/poleras/page.tsx
@@ -0,0 +1,35 @@
+import { About } from "@/app/components/shared/landing/About";
+import { ClientSection } from "@/app/components/shared/landing/ClientSection";
+import { Layout } from "@/app/layout/Layout";
+import { products, sliderMale } from "./Data";
+import { SliderHeroShoes } from "../zapatillas/components/SliderHeroShoes";
+import SliderShoes from "../zapatillas/components/SliderShoes";
+
+export default function Shoes() {
+  console.log(sliderMale);
+
+  return (
+    <Layout>
+      <div className="my-[150px]">
+        <SliderHeroShoes shoes={sliderMale} />
+        <ClientSection
+          image="/shoes/preview.png"
+          theme="primary"
+          title="Zapatillas Personalizadas"
+          color="#309FFF"
+        />
+        <SliderShoes
+          tag="detalles"
+          color="#309FFF"
+          theme="primary"
+          products={products}
+        />
+        <About
+          title="Sobre Nosotros"
+          content="Tenemos los mas grandes artistas del sector para confeccionar sus zapatillas personalizadas de la mayor calidad posible"
+          image="/shoes/preview.png"
+        />
+      </div>
+    </Layout>
+  );
+}
