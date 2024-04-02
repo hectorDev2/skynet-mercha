5114128a2bf2b879d1476f6d4630247b3ef7ee72
Hector Barazorda <ph.barazorda@gmail.com>
2024-04-02 16:16:06 -0500

    feat: :art: type of jackets and shoes

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-export interface tShirts {
+export interface tShirt {
   id: string;
   name: string;
   category: string;
@@ -6,4 +6,25 @@ export interface tShirts {
   price: number;
   description: string;
   images: any[];
+}
+
+export interface Shoes {
+  id: string;
+  name: string;
+  category: string;
+  subcategory: string;
+  price: number;
+  description: string;
+  images: any[];
+  gender: string;
+}
+
+export interface Jacket {
+  id: string;
+  name: string;
+  category: string;
+  price: number;
+  description: string;
+  images: any[];
+  gender: string;
 }
