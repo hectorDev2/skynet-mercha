ca858be1747166b9f46b9b460ec47a1cfe2def79
Hector Barazorda <ph.barazorda@gmail.com>
2024-04-02 15:29:16 -0500

    update CardProductEx.tsx, Banner.tsx and page.tsx

--- src/app/components/shared/Banner.tsx
+++ src/app/components/shared/Banner.tsx
@@ -1,44 +1,41 @@
-import React from 'react'
+import React from "react";
 
-export const Banner = () => {
+export const Banner = ({
+  children,
+  image = "mario.png",
+  bgImage = "https://cdn.wallpapersafari.com/87/90/GjYlhA.jpg",
+}: {
+  children: React.ReactNode;
+  image?: string;
+  bgImage?: string;
+}) => {
   return (
     <>
-      <section className='w-full'>
+      <section className="w-full">
         <div
-          className='bg-yellow-50 h-full w-full rounded-primary relative flex flex-col gap-y-8 lg:p-24 p-8'
+          className="bg-yellow-50 h-full w-full rounded-primary relative flex flex-col gap-y-8 lg:p-24 p-8"
           style={{
-            backgroundImage:
-              'url("https://cdn.wallpapersafari.com/87/90/GjYlhA.jpg")',
-            backgroundAttachment: 'fixed',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center'
+            backgroundImage: `url(${bgImage})`,
+            backgroundAttachment: "fixed",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
         >
           <img
-            alt='model'
-            loading='lazy'
-            decoding='async'
-            data-nimg='1'
-            className='w-[300px] lg:absolute bottom-0 left-0 order-2'
-            style={{ color: 'transparent' }}
-            src='mario.png'
+            alt="model"
+            loading="lazy"
+            decoding="async"
+            data-nimg="1"
+            className="w-[300px] lg:absolute bottom-0 left-0 order-2"
+            style={{ color: "transparent" }}
+            src={`${image}`}
           />
-          <article className='flex flex-col justify-start items-end order-1'>
-            <div className='flex flex-col gap-y-4 z-50  '>
-              <h1 className='md:text-6xl drop-shadow-lg text-4xl'>
-                Especiales ofertas por lanzamiento
-              </h1>
-              <p className='flex flex-row gap-x-0.5 items-center text-lg text-[#04ADFF]'>
-                Exclusivos para gente gammer
-              </p>
-              <button className='px-8 py-4 border border-black rounded-secondary bg-black hover:bg-black/90 text-white transition-colors drop-shadow w-fit mt-4'>
-                Descubre nuestros modelos
-              </button>
-            </div>
+          <article className="flex flex-col justify-start items-end order-1">
+            {children}
           </article>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import { Download } from './components/containers/Download'
-import { Hero } from './components/containers/Hero'
-import { Layout } from './layout/Layout'
-import { Banner } from './components/shared/Banner'
-import { ProductContainer } from './components/ProductContainer'
+import Head from "next/head";
+import { Download } from "./components/containers/Download";
+import { Hero } from "./components/containers/Hero";
+import { Layout } from "./layout/Layout";
+import { Banner } from "./components/shared/Banner";
+import { ProductContainer } from "./components/ProductContainer";
 
-export default async function Home () {
+export default async function Home() {
   return (
     <Layout>
       <Head>
@@ -14,9 +14,24 @@ export default async function Home () {
       <main>
         <Hero />
         <ProductContainer />
-        <Banner />
+        <Banner
+          image="mario.png"
+          bgImage="https://cdn.wallpapersafari.com/87/90/GjYlhA.jpg"
+        >
+          <div className="flex flex-col gap-y-4 z-50  ">
+            <h1 className="md:text-6xl drop-shadow-lg text-4xl">
+              Especiales ofertas por lanzamiento
+            </h1>
+            <p className="flex flex-row gap-x-0.5 items-center text-lg text-[#04ADFF]">
+              Exclusivos para gente gammer
+            </p>
+            <button className="px-8 py-4 border border-black rounded-secondary bg-black hover:bg-black/90 text-white transition-colors drop-shadow w-fit mt-4">
+              Descubre nuestros modelos
+            </button>
+          </div>
+        </Banner>
         <Download />
       </main>
     </Layout>
-  )
+  );
 }
